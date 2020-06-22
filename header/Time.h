89055ec39e9ec8e8957b23442091b77adffc1790
Sascha Dostal <91dosa1bif@c-143-002.ad.hft-stuttgart.de>
2020-06-22 16:03:58 +0200

    add class Time

---
+++ header/Time.h
@@ -0,0 +1,18 @@
+//
+// Created by 91dosa1bif on 22.06.20.
+//
+
+#ifndef ABSCHLUSSAUFGABE_TIME_H
+#define ABSCHLUSSAUFGABE_TIME_H
+
+class Time {
+public:
+    Time(int day, int min);
+private:
+    // Prüfungstage 1-10; day = 0, wenn Zeitdifferenz, z.B. Prüfungsdauer
+    int day;
+    // Minute 0-599; Minute 0 entspricht 8 Uhr, Minute 600 entspricht 18 Uhr
+    int min;
+};
+
+#endif //ABSCHLUSSAUFGABE_TIME_H
---
+++ src/Time.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 91dosa1bif on 22.06.20.
+//
+
+#include "../header/Time.h"
