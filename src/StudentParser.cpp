126bf9078eabba36c406654c5055ba47c0d61f66
Felix Koerner <56401181+ByteSquire@users.noreply.github.com>
2020-06-27 22:19:49 +0200

    switch to unordered_map, don't check if exam exists

--- src/StudentParser.cpp
+++ src/StudentParser.cpp
@@ -4,78 +4,62 @@
 
 #include <iostream>
 #include "../header/StudentParser.h"
-#include "../header/CSVParser.h"
-#include "../header/ExamParser.h"
 using namespace std;
 
-StudentParser::StudentParser(string fileName, std::vector<ExamParser::Exam> allExams) : CSVParser(fileName, separator::semicolon){
+StudentParser::StudentParser(string fileName) : CSVParser(fileName, separator::semicolon) {
     tableData tmp = getData();
-    for(int i=1; i<tmp.size(); i++){
-        Student student = {};
+
+    vector<string> fieldsOfStudy = getColumn(1);
+    vector<string>::iterator ip;
+    ip = unique(fieldsOfStudy.begin(), fieldsOfStudy.end());
+    fieldsOfStudy.resize(distance(fieldsOfStudy.begin(), ip));
+
+    for (int i = 1; i < fieldsOfStudy.size(); i++) {
+        unordered_map<string, ExamParser::Exam> a = {pair<string, ExamParser::Exam>("", ExamParser::Exam())};
+        unordered_map<int, unordered_map<string, ExamParser::Exam>> tmp1 = {pair<int, unordered_map<string, ExamParser::Exam>>(0,a)};
+        pair<string, unordered_map<int, unordered_map<string, ExamParser::Exam>>> fieldOfStudyDefiner(fieldsOfStudy.at(i),tmp1);
+        students.insert(fieldOfStudyDefiner);
+    }
+
+    for (int i = 1; i < tmp.size(); i++) {
         vector<string> studentData = getRow(i);
-        bool studExists = false;
+
+        // Setze Matrikelnummer, Studiengang
+        int matrikelNumber = stoi(studentData.at(0));
+        string fieldOfStudy = studentData.at(1);
+
         // Test ob Student bereits existiert
-        for(Student& stud : students){
-            if(stoi(studentData.at(0)) == stud.matrikelNumber){
-                studExists = true;
-            }
-        }
-        // Wenn Student nicht existiert, setze Matrikelnummer, füge Prüfung zu Prüfungen hinzu
-        // und füge Student zum Student-Vector hinzu
-        if(!studExists){
-            student.matrikelNumber = stoi(studentData.at(0));
-            student.fieldOfStudy = studentData.at(1);
-            bool examExists = false;
-            // Füge dem Student die Prüfung zu der die Anmeldung gehört
-            for(ExamParser::Exam& e : allExams){
-                if(stoi(studentData.at(3)) == e.examNumber  && stoi(studentData.at(2)) == e.examVersion
-                    && (e.fieldOfStudy.compare(studentData.at(1)) == 0)){
-                    student.exams.push_back(e);
-                    examExists = true;
-                }
-            }
-            // Wenn die Klausur in der Anmeldung existiert, dann füge Student zur Liste der Studenten hinzu
-            if(examExists){
-                students.push_back(student);
-            // Wenn dazugehörige Klausur nicht existiert gebe aus, dass Anmeldung ignoriert wurde
-            } else {
-                cout << "Registration ignored: Exam with pnumber " << stoi(studentData.at(3)) << ", version " << studentData.at(2)
-                     << " stg " << studentData.at(1) << " does not exist." << endl;
-            }
+        bool studExists = (students.at(fieldOfStudy).count(matrikelNumber));
+
         // Wenn Student existiert, suche Student in Student-Vector und füge Prüfung hinzu
+        if (studExists) {
+            ExamParser::Exam exam = ExamParser::Exam{studentData.at(1), "", stoi(studentData.at(2)), stoi(studentData.at(3))};
+            string key = exam.fieldOfStudy.append(to_string(exam.examVersion)).append(to_string(exam.examNumber));
+            students.at(fieldOfStudy).at(matrikelNumber).insert({key, exam});
         } else {
-            for(Student& stud : students){
-                if(stoi(studentData.at(0)) == stud.matrikelNumber){
-                    bool examExists = false;
-                    for(ExamParser::Exam& e : allExams){
-                        if((stoi(studentData.at(3)) == e.examNumber) && (stoi(studentData.at(2)) == e.examVersion)
-                            && (e.fieldOfStudy.compare(studentData.at(1)) == 0)){
-                            stud.exams.push_back(e);
-                            examExists = true;
-                        }
-                    }
-                    // Wenn dazugehörige Klausur nicht existiert gebe aus, dass Anmeldung ignoriert wurde
-                    if(!examExists){
-                        cout << "Registration ignored: Exam with pnumber " << stoi(studentData.at(3)) << ", version " << studentData.at(2)
-                        << " stg " << studentData.at(1) << " does not exist." << endl;
-                    }
-                }
-            }
+            // Füge dem Student die Prüfung zu der die Anmeldung gehört hinzu
+            ExamParser::Exam exam = ExamParser::Exam{studentData.at(1), "", stoi(studentData.at(2)), stoi(studentData.at(3))};
+            string key = exam.fieldOfStudy.append(to_string(exam.examVersion)).append(to_string(exam.examNumber));
+            students.at(fieldOfStudy).insert({matrikelNumber, {pair<string, ExamParser::Exam>(key, exam)}});
         }
     }
 }
 
 StudentParser::~StudentParser() = default;
 
-vector<StudentParser::Student> &StudentParser::getStudents() {
+unordered_map<string, unordered_map<int, unordered_map<string, ExamParser::Exam>>> StudentParser::getStudents() {
     return students;
 }
 
-bool StudentParser::testTime(Time t, StudentParser::Student s){
-    for(ExamParser::Exam& e : s.exams){
-        if(!Time::diff(t, e.examTime, 240)){
+bool StudentParser::testTime(Time t, string fieldOfStudy, int matrikelNumber){
+    for(auto& element : students.at(fieldOfStudy).at(matrikelNumber)){
+        if(!Time::diff(t, element.second.examTime, 240)){
             return false;
         }
     }
     return true;
+}
+
+void StudentParser::removeExam(std::string fieldOfStudy, int matrikelNumber, std::string key) {
+    students.at(fieldOfStudy).at(matrikelNumber).erase(key);
 }
