94b3214045ec378a3807da4081db368804f0a420
Felix Koerner <56401181+ByteSquire@users.noreply.github.com>
2020-06-28 08:10:25 +0200

    fix endless loop, code cleanup, output error to cerr

--- src/main.cpp
+++ src/main.cpp
@@ -25,33 +25,38 @@ int main() {
     vector<ExamParser::Exam> notPlannedExams;
 
     string studentFile = "../InputData/Anmeldungen_WS2019_KL.csv";
-    StudentParser studentParser(studentFile);
+    StudentParser studentParser(studentFile, allExams);
 
     unordered_map<string, unordered_map<int, unordered_map<string, ExamParser::Exam>>> students = studentParser.getStudents();
 
     // TODO Sortieren der Prüfungen von "allExams" nach examLength
 
     // Schleife um allen Prüfungen einen Termin zuzuordnen
-    loop:
+    int examsWithoutStudents = 0;
     for(auto& e : allExams) {
         bool run = true;
         if (e.second.planned) run = false;  // Wenn die Klausur bereits geplant ist, keinen Termin suchen
         int day = 1;
         int min = 0;
         // Schleife für Terminsuche
+        loop:
         while (run) {
             bool valid = true;
             // Schleife um Gültigkeit eines Termins am Tag "day" um Zeit "min" bei allen Studenten des Studiengangs zu prüfen
             vector<int> studentsParticipating;
             for (auto& students_ : students.at(e.second.fieldOfStudy)) {
                 // Wenn Student die Prüfung schreibt
-                if (students_.second.count(e.first)) {
+                if (students_.second.contains(e.first)) {
                     studentsParticipating.push_back(students_.first);
                     // Wenn der Student keine Zeit hat -> ungütiger Termin
                     if (!studentParser.testTime(Time(day, min, e.second.examLength), e.second.fieldOfStudy, students_.first)) valid = false;
                 }
             }
-            if(studentsParticipating.empty()) goto loop;
+            if(studentsParticipating.empty()) {
+                examsWithoutStudents++;
+                run = false;
+                goto loop;
+            }
             // Wenn "valid", dann getRoomsForNStudents()
             if (valid) {
                 vector<RoomParser::Room> examRooms = RoomParser::getRoomsForNStudents(studentsParticipating.size(),
@@ -60,10 +65,10 @@ int main() {
                 // Wenn getRoomsForNStudents() kein Raum zurückgibt -> valid = false;
                 if (examRooms.empty()) {
                     goto elseBlock;
-                    // Wenn getRoomsForNStudents() Räume zurück gibt, dann plane Termin in Prüfung
-                    // Prüfung in Liste room->exams und in String exam->room eintragen
+                // Wenn getRoomsForNStudents() Räume zurück gibt, dann plane Termin in Prüfung
                 } else {
                     e.second.examTime = Time(day, min, e.second.examLength);
+                    // Prüfung in Liste room->exams und in String exam->room eintragen
                     for (RoomParser::Room &r1 : examRooms) {
                         for (RoomParser::Room &r2 : biggestNRooms) {
                             if (r1.location == r2.location) r2.exams.push_back(e.second);
@@ -73,6 +78,7 @@ int main() {
                     }
                 }
 
+                // Wenn der Termin gültig ist, dann plane den Termin
                 cout << "Time scheduled: Day: " << e.second.examTime.day << " Time: " << std::setfill('0')
                      << setw(2) << (int) e.second.examTime.min / 60 + 8 << ":" << setw(2) << e.second.examTime.min % 60
                      << ", duration " << e.second.examTime.duration << " Exam: " << e.second.examNumber << " "
@@ -96,22 +102,26 @@ int main() {
                 if (day > 10) day = 1;
                 // Wenn alle 10 Tage durchlaufen wurden ohne dass ein Termin gefunden wurde, Exam der Liste notPlannedExams hinzufügen
                 if (day == 1 && min == 0) {
-                    cout << "Error: No valid time found for Exam!" << endl;
+                    cerr << "Error: No valid time found for Exam!" << endl;
                     notPlannedExams.push_back(e.second);
                     run = false;
                 }
-                // Wenn der Termin gültig ist, dann plane den Termin
             }
         }
     }
 
-    cout << "Not plannable exams: " << endl;
-    for(const ExamParser::Exam& ex : notPlannedExams){
-        cout << ex.examName << "Field of study: " << ex.fieldOfStudy << " Number: " << ex.examNumber << " Version: "
-            << ex.examVersion << endl;
-    }
     cout << "Number of not plannable exams : " << notPlannedExams.size() << endl;
+    if (!notPlannedExams.empty()){
+        cout << "Not plannable exams: " << endl;
+        for(const ExamParser::Exam& ex : notPlannedExams){
+            cout << ex.examName << "Field of study: " << ex.fieldOfStudy << " Number: " << ex.examNumber << " Version: "
+                 << ex.examVersion << endl;
+        }
+    }
 
+    cout << "Number of exams without students: " << examsWithoutStudents << endl;
+    
+    // TODO Sortieren der Prüfungen von "allExams" nach examTime (wie unten nur jetzt eben mit der unordered_map)
     /*std::sort(allExams.begin(), allExams.end(),
               [](const ExamParser::Exam& a, const ExamParser::Exam& b)
                 { return (a.examTime.day == b.examTime.day)? (a.examTime.min > b.examTime.min) : (a.examTime.day > b.examTime.day);});*/
