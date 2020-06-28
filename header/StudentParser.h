6c001d4fbd5b3c757cb55cecddca0ad24fd7910b
Felix Koerner <56401181+ByteSquire@users.noreply.github.com>
2020-06-28 12:03:20 +0200

    self-implement unique function

--- header/StudentParser.h
+++ header/StudentParser.h
@@ -6,6 +6,7 @@
 #define ABSCHLUSSAUFGABE_STUDENTPARSER_H
 
 #include <unordered_map>
+#include <vector>
 #include "CSVParser.h"
 #include "ExamParser.h"
 #include "Time.h"
@@ -18,6 +19,8 @@ public:
     bool testTime(Time t,  std::string fieldOfStudy, int matrikelNumber);
 private:
     std::unordered_map<std::string,std::unordered_map<int, std::unordered_map<std::string, ExamParser::Exam>>> students;
+
+    std::vector<std::string> unique(const std::vector<std::string>& input);
 };
 
 #endif //ABSCHLUSSAUFGABE_STUDENTPARSER_H
